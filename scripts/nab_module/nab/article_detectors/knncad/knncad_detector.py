1e7eb16f775cfbd7dfa0aef3c81e3f4d5617a26d
ish-vlad <ishvlad28@gmail.com>
2017-01-17 02:49:02 +0300

    (1,1) speedup

--- scripts/nab_module/nab/article_detectors/knncad/knncad_detector.py
+++ scripts/nab_module/nab/article_detectors/knncad/knncad_detector.py
@@ -26,20 +26,27 @@ class KnncadDetector(AnomalyDetector):
         diff = a - np.array(b)
         return np.sqrt(np.dot(np.dot(diff, self.sigma_inv), diff.T))
 
-    def get_NN_dist(self, item, array=None):
-        if array is None:
-            array = self.training[:-1]
+    def update_sigma(self, new_item, inverse=False):
+        if self.record_count == self.probationaryPeriod - self.dim:
+            inverse = True
 
-        dists = []
-        for x in array:
-            dist = self.metric(x, item)
-            if len(dists) < self.k:
-                dists.append(dist)
+        try:
+            if inverse:
+                self.mean = np.mean(self.training, axis=0).reshape(-1, 1)
+                X = self.training - self.mean.T
+                self.sigma = np.dot(X.T, X)
             else:
-                i = np.argmax(dists)
-                if dists[i] > dist:
-                    dists[i] = dist
-        return sum(dists) / (self.rang * self.k * self.dim ** 0.5)
+                delta_ = np.array([new_item]) - self.mean.T
+                self.mean += delta_.T / self.record_count
+
+                U = np.dot(delta_.T, delta_)
+                U -= U / self.record_count
+                self.sigma += U
+
+            self.sigma_inv = np.linalg.inv(self.sigma)
+            self.sigma_inv /= np.linalg.norm(self.sigma_inv, axis=0)
+        except np.linalg.linalg.LinAlgError:
+            print('Singular Matrix at record', self.record_count)
 
     def update_sigma(self, new_item, inverse=False):
         if self.record_count == self.probationaryPeriod - self.dim:
